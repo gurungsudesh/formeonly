532fd4ac37e68e280158811a2d983d117daf3a0a
gurungSudesh <gurung.sudesh@yahoo.com>
2018-08-11 08:09:03 +0545

    I modified the file in the Windows

--- example1.java
+++ example1.java
@@ -1,5 +1,7 @@
 class example1{
     public static void main(Strig [] args){
         System.out.println("This is the print from Mac version.");
+        System.out.println("Pulled in my windows");
+        System.out.println("Add this message from my windows.");
     }
 }
